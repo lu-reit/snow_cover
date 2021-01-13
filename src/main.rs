a1d89f8f8befe7307ea9fd33f51c6b6aa40be57e
Lukas Reitemeyer <lukas@skoni.de>
2021-01-13 19:44:47 +0100

    Got netcdf to build, created gitignore

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
+use netcdf;
+
+fn main() {
+    let netcdf_file = netcdf::open("../test.nc");
+    let mut img = ImageBuffer::from_fn(512, 512, |x, y| {
+        if x % 2 == 0 {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+
+//    match img.save("test.png") {
+ //       Ok(_) => {},
+        
+}
