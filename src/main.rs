699ae7fbc4eaee07024b13706b3f020358424be0
Lukas Reitemeyer <lukas@skoni.de>
2021-01-13 21:55:41 +0100

    Figured out how to actuaclly work with netcdf-files

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
 use netcdf;
 
 fn main() {
-    let netcdf_file = netcdf::open("../test.nc");
+    let netcdf_file = netcdf::open(
+        "test.nc"
+        ).unwrap();
+    for dim in netcdf_file.variables() {
+        println!("{:?}", dim.name());
+    }
+
     let mut img = ImageBuffer::from_fn(512, 512, |x, y| {
         if x % 2 == 0 {
             image::Luma([0u8])
